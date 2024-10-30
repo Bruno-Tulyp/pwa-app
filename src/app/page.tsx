39b9543c10f6ee670e55dc2bab5477dd9fec568e
Erdal Karaer <karaer.erd@gmail.com>
2024-10-30 16:16:14 +0100

    Ajout dashboard + .env + page profil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-const Page = () => null
+import Dashboard from "../components/dashboard"
+//import Profile from "../components/my_profile"
 
-export default Page
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <Dashboard />
+    </div>
+  )
+}
+
+export default Home
